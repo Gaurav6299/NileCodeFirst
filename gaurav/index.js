e6beb837b805aa544d946d4fe47a83fa52b1924e
Gaurav6299 <kumargaurav6299@gmail.com>
2021-11-23 13:20:24 +0000

    object

---
+++ gaurav/index.js
@@ -0,0 +1,23 @@
+let x=10;
+
+{
+    let x=20;
+}
+console.log(x);
+
+
+// Arrays
+// const arr=[1,2,3,"gaurav"]
+// arr[0]="kumar"
+// arr[3]='aman'
+// arr[2]="True"
+// arr.push("payal")
+// arr.push("Ankt")
+// arr.push("vikash")
+// console.log(arr)
+
+// Objects
+const car={1:"gaurav",title:"mishra",name:"payal"}
+car.title="Mishra"
+car.name="Aman"
+console.log(car)
